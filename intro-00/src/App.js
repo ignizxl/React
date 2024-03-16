51d8b47561b278481e5d7f6c0a82bf0f6dea0c35
ignizxl <igor.joao@academico.ifpb.edu.br>
2024-03-16 19:36:07 -0300

    aula-02

---
+++ intro-00/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  const name    = 'João Igor';
+  const newName = name.toLocaleUpperCase();
+  const url     = "https://via.placeholder.com/150";
+  
+  function somarDoisNumeros(n1, n2){
+    return n1 + n2;
+  }
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>Este é o meu primeiro App.</p>
+      <p>Alterando o JSX</p>
+      <p>Olá, {name}!</p>
+      <p>Olá de novo, {newName}!</p>
+      <p>A soma entre {10} e {14} é igual a {somarDoisNumeros(10, 14)}</p>
+      <img src={url} alt="minha imagem"></img>
+    </div>
+  );
+}
+
+export default App;
