93f7138db2b31c3f3101d70c5aa6c238164915f9
ignizxl <igor.joao@academico.ifpb.edu.br>
2024-03-17 16:57:39 -0300

    aula-05

--- intro-00/src/components/Frase.js
+++ intro-00/src/components/Frase.js
@@ -1,7 +1,11 @@
+// na hora de importar o css a nivel de componente eu preciso utilizar o 'styles' from
+// caminho
+import styles from './Frase.module.css'; 
 function Frase(){
     return (
-        <div>
-            <p>Este é um componente com uma frase!</p>
+        <div className={styles.fraseContainer}>
+            <p className={styles.fraseContent}>Este é um componente com uma frase!</p>
+            <p className={styles.fraseContent}>Testando a estilização a nível de componente!</p>
         </div>
     );
 }
