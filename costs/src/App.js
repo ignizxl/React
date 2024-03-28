4a060c8bb1387aeff179945cfc38e5e72ff738a2
ignizxl <igor.joao@academico.ifpb.edu.br>
2024-03-28 19:22:02 -0300

    aula-18 - NavBar e Footer

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,29 +1,29 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
+
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </ul>
-      <Container customClass="min-height">
+      <NavBar></NavBar>
+      <Container customClass="min-hight">
         <Routes>
             <Route exact path='/' element={<Home></Home>}/>
+            <Route exact path='/projects' element={<Projects></Projects>}/>
             <Route exact path='/company' element={<Company></Company>}/>
             <Route exact path='/contact' element={<Contact></Contact>}/>
             <Route exact path='/newproject' element={<NewProject></NewProject>}/>
         </Routes>
       </Container>
-      <p>Footer</p>
+     <Footer></Footer>
     </Router>
   );
 }
